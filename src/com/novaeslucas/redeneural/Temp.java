2218425c3eb2a09f401bf44e6963cbd7a18dc76a
Lucas Novaes <novaeslucas@gmail.com>
2019-08-08 17:05:47 -0300

    update

--- src/com/novaeslucas/redeneural/Temp.java
+++ src/com/novaeslucas/redeneural/Temp.java
@@ -5,14 +5,6 @@ public class Temp {
     //1 - Criar matriz com numeros randomicos
     public static void main(String[] args) {
         NeuralNetwork nn = new NeuralNetwork(2,3,2);
-        System.out.println("Bias input hidden");
-        nn.getBiasInputHidden().print();
-        System.out.println("Bias hidden output");
-        nn.getBiasHiddenOutput().print();
-        System.out.println("Weigth input hidden");
-        nn.getWeigthInputHidden().print();
-        System.out.println("Weigth hidden output");
-        nn.getBiasHiddenOutput().print();
         System.out.println("Inputs");
         double[][] inputs = {{1.0},{1.0}};
         print(inputs);
