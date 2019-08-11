5a5f9620a1a61255ceaa8e684b63d5693b4fe7f9
novaeslucas <novaeslucas@gmail.com>
2019-08-11 01:16:16 -0300

    update

--- src/com/novaeslucas/redeneural/Main.java
+++ src/com/novaeslucas/redeneural/Main.java
@@ -3,7 +3,7 @@ package com.novaeslucas.redeneural;
 public class Main {
 
     public static void main(String[] args) {
-        NeuralNetwork nn = new NeuralNetwork(2,3,1);
+        NeuralNetwork nn = new NeuralNetwork(2,1000,1);
         //INPUTS
         double[][] inputs = {{1.0,1.0},{1.0,0.0},{0.0,0.1},{0.0,0.0}};
         //TARGETS
@@ -13,20 +13,11 @@ public class Main {
         double[] arr2 = {1.0,0.0};
         double[] arr3 = {1.0,1.0};
         double[] arr4 = {0.0,1.0};
-        boolean train = true;
-        int k = 0;
-        while (train){
-            for (int i = 0; i < 100000; i++){
-                int index = (int) Math.floor(Math.random()*4);
-//                System.out.println("index = " + index);
-//                System.out.println("inputs["+index+"] = "+inputs[index][0]+","+inputs[index][1]+" = "+target[index][0]);
-                nn.train(inputs[index], target[index]);
-            }
-            if(nn.predict(arr2)[0] > 0.98){
-                train = false;
-                System.out.println("terminou iteracao " + k);
-            }
-            k++;
+
+        for (int i = 0; i < 100000; i++){
+            int index = (int) Math.floor(Math.random()*4);
+            nn.train(inputs[index], target[index]);
         }
+        System.out.println(nn.predict(arr1)[0]);
     }
 }
